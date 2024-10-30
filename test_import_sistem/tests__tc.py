96de0c879d732e315bf98a76c888647f23bd7541
Tatiana Yurchenko <taf88156@mail.ru>
2024-10-30 16:23:11 +0300

    refactor: apply pydantic

--- test_import_sistem/tests__tc.py
+++ test_import_sistem/tests__tc.py
@@ -1,50 +1,28 @@
 import pytest
 import requests
 from faker import Faker
+from pydantic import BaseModel, Field
+from enum import Enum
 
 faker = Faker()
 
 URL = 'http://127.0.0.1:8000/testcases/'
-data = {
-    "id": faker.random_number(3),
-    "name": faker.text(8),
-    "description": faker.sentence(6),
-    "steps": [
-        "string"
-    ],
-    "expected_result": "string",
-    "priority": "низкий"
-}
-data2 = {
-    "id": faker.random_number(4),
-    "name": faker.text(8),
-    "description": faker.sentence(6),
-    "steps": [
-        "string"
-    ],
-    "expected_result": "string",
-    "priority": "высокий"
-}
-data3 = {
-    "id": faker.random_number(4),
-    "name": faker.lexify(text="?" * 101),
-    "description": faker.sentence(6),
-    "steps": [
-        "string"
-    ],
-    "expected_result": "string",
-    "priority": "высокий"
-}
-data4 = {
-    "id": faker.random_number(4),
-    "name": faker.lexify(text="?" * 99),
-    "description": faker.lexify(text="?" * 1001),
-    "steps": [
-        "string"
-    ],
-    "expected_result": "string",
-    "priority": "высокий"
-}
+
+class Priority(str, Enum):
+    low = "низкий"
+    medium = "средний"
+    high = "высокий"
+
+
+class Data_case(BaseModel):
+    id: int = Field(default=faker.random_number(4), ge=0, le=120)
+    name: str = Field(default=faker.text(8))
+    description: str = Field(default=faker.sentence(6))
+    steps: list[str] = Field(default=[faker.text(8)])
+    expected_result: str = Field(default=faker.sentence(6))
+    priority: Priority = Field(default=Priority.medium)
+
+data = Data_case().model_dump()
 def create_tc():
     response = requests.post(URL, json=data)
     id = response.json()['id']
@@ -55,12 +33,15 @@ def test_create_tc():
     assert data['id'] == response.json()['id'] and data["name"] == response.json()["name"]
 
 def test_create_tc_negative():
-    response = requests.post(URL, json=data3)
+    name = faker.lexify(text="?" * 101)
+    response = requests.post(URL, json=Data_case(name=name).model_dump())
     assert response.json()['detail'][0]['type'] == 'string_too_long'
 
 def test_create_tc_negative_description():
-    response = requests.post(URL, json=data4)
+    description = faker.lexify(text="?" * 1001)
+    response = requests.post(URL, json=Data_case(description=description).model_dump())
     assert response.json()['detail'][0]['type'] == 'string_too_long'
+
 
 def test_get_tc():
     create_tc()
@@ -72,6 +53,7 @@ def test_get_tc_by_id():
     assert response.status_code == 200
 
 def test_update_tc():
+    data2 = Data_case().model_dump()
     response = requests.put(URL+f'{create_tc()}', json=data2)
     assert data2['id'] == response.json()['id'] and data2["name"] == response.json()["name"]
 
